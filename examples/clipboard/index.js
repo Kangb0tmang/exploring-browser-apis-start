c712f949a53fb942b26c1a0f22cf98d23845cf55
Kangb0tmang <jon.kang.jd@gmail.com>
2023-01-18 17:59:55 +1100

    Copy/Paste with permissions

---
+++ examples/clipboard/index.js
@@ -0,0 +1,19 @@
+const copy_input = document.getElementById("copy-input");
+const copy_button = document.getElementById("copy");
+
+copy_button.addEventListener("click", async () => {
+  let value = copy_input.value;
+  await navigator.clipboard.writeText(value);
+});
+
+const paste_input = document.getElementById("paste-input");
+const paste_button = document.getElementById("paste");
+
+if ("readText" in navigator.clipboard) {
+  paste_button.disabled = false;
+}
+
+paste_button.addEventListener("click", async () => {
+  const value = await navigator.clipboard.readText();
+  paste_input.value = value;
+});
