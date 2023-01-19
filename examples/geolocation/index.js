a4a4af29f501f40335ad143c7a09055b041fdf89
Kangb0tmang <jon.kang.jd@gmail.com>
2023-01-19 17:57:47 +1100

    Location, Google always knows...

---
+++ examples/geolocation/index.js
@@ -0,0 +1,18 @@
+const card = document.querySelector('.card');
+const map = document.getElementById('map');
+
+if ('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      map.src = `https://maps.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}&output=embed`;
+    },
+    (err) => {
+      map.remove();
+      let errorMessage = document.createElement('p');
+      errorMessage.textContent = `Error: ${err.message}`;
+      card.appendChild(errorMessage);
+    }
+  );
+} else {
+  map.remove();
+}
