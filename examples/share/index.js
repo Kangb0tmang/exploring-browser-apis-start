2b5c4946d9cacad5a1b610ba2aaf4921e6278b35
Kangb0tmang <jon.kang.jd@gmail.com>
2023-01-19 15:20:27 +1100

    Share desktop fallback

--- examples/share/index.js
+++ examples/share/index.js
@@ -1,6 +1,8 @@
 // Share API limited for now, but works better on mobile
 
 const share_button = document.getElementById("share");
+const share_link = document.getElementById("share-link");
+const text = document.getElementById("text");
 
 if ("share" in navigator) {
   share_button.disabled = false;
@@ -12,14 +14,13 @@ share_button.addEventListener("click", async () => {
   const text = document.getElementById("text").value;
   const shareData = {
     title: "Shared from Level Up",
-    text,
+    text: text.value,
     url: "https://leveluptutorials.com",
   };
   await navigator.share(shareData);
 });
 
 let isMobileAgent;
-
 if (
   /Android|webOS|iPhone|iPad|Opera Mini|BlackBerry/i.test(navigator.userAgent)
 ) {
@@ -28,13 +29,29 @@ if (
   isMobileAgent = false;
 }
 
-share_button.innerText = isMobileAgent;
-
 // Check screen size
 const isMobileSize = window.matchMedia(
   "only screen and (max-width: 760px)"
 ).matches;
 
 // Check for touch events
-const isTouch = "ontouchstart" in document.document.element;
-share_button.innerText = isTouch;
+const isTouch = "ontouchstart" in document.documentElement;
+
+// Initialise
+if ("share" in navigator) {
+  // Check if mobile
+  if (isMobileAgent && isMobileSize && isTouch) {
+    // Make button active
+    share_button.disabled = false;
+    share_link.style.display = "none";
+  } else {
+    intialiseFallback();
+  }
+} else {
+  intialiseFallback();
+}
+
+function intialiseFallback() {
+  share_button.style.display = "none";
+  share_link.href = `https://twitter.com/share?url=${window.location.href}&text=${text.value}`;
+}
